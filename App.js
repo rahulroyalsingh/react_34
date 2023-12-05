9053c319ec5d6314295a7911d3e1901180b01ec8
rahul singh <116128750+rahulroyalsingh@users.noreply.github.com>
2023-12-05 16:44:19 +0530

    react-hook_useMemo
    by using usememo function we can reduce unwanted calling, which can improve app response

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import './App.css';
 function App() {
   const [count, setCount] = useState(0);
   const [item, setItem] = useState(5);
 
-
+const multiCountMemo=useMemo(
+  function multicount()
+{
+  console.warn("multiCount")
+  return count *6
+},[count]
+)
   return (
     <div className="App">
       <h1>useMemo React hook</h1>
       <h2>Count :{count}</h2>
       <h2>Item :{item}</h2>
-      
+      <h2>{multiCountMemo}</h2>
 
       <button onClick={() => setCount(count + 1)}>Update Count</button>
       <button onClick={() => setItem(item * 5)}>Update Item</button>
